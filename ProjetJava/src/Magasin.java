2c60a37b2206cc36a9bbc802bfb34e7a1c230a1a
Rebecca <67113677+BeccaMorel@users.noreply.github.com>
2022-12-09 10:27:15 +0100

    Update Magasin.java
    ajouter constructeur avec nom proprio

--- ProjetJava/src/Magasin.java
+++ ProjetJava/src/Magasin.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Magasin extends Batiment{
+    ArrayList<Instrument> instruments;
+    Personne proprio;
+
     public Magasin(){
         super();
+        instruments = new ArrayList<Instrument>();
     }
+    public Magasin(Personne proprietaire){
+        super();
+        instruments = new ArrayList<Instrument>();
+        this.proprio = proprietaire;
+    }
+
 }
