0adf107ec0953cebf5d4ab4e2d2532d6f918badb
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2022-12-09 09:46:10 +0100

    class intru + personne

---
+++ ProjetJava/src/Instrument.java
@@ -0,0 +1,13 @@
+public class Instrument {
+    String nom;
+    float prixAchat;
+    float prixVente;
+
+    public Instrument() {
+    }
+    public Instrument(String nom, float prixAchat, float prixVente) {
+        this.nom = nom;
+        this.prixAchat = prixAchat;
+        this.prixVente = prixVente;
+    }
+}
---
+++ ProjetJava/src/Personne.java
@@ -0,0 +1,10 @@
+public class Personne {
+    String nom;
+    String prenom;
+    public Personne() {
+    }
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
