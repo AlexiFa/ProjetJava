015767a3d15a6ebb39d0471a55125316de0c9b03
Rebecca <67113677+BeccaMorel@users.noreply.github.com>
2022-12-09 09:13:58 +0100

    batiment classes créés

---
+++ ProjetJava/src/Magasin.java
@@ -0,0 +1,5 @@
+public class Magasin extends Batiment{
+    public Magasin(){
+
+    }
+}
--- ProjetJava/src/Main.java
+++ ProjetJava/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println();
+        Batiment hot = new Hotel();
+        Maison mai = new Maison();
+        Immeuble immeuble = new Immeuble();
+        Magasin mag = new Magasin();
+        System.out.println("batiment classes created.");
+
     }
 }
