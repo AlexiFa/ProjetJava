e6ef2f94d523dbbafff365ad568656082cdf41da
Rebecca <67113677+BeccaMorel@users.noreply.github.com>
2022-12-11 15:10:09 +0100

    Update Main.java
    Starting to comment everything

--- ProjetJava/src/Main.java
+++ ProjetJava/src/Main.java
@@ -1,6 +1,10 @@
 import Buildings.*;
-import Instrument.*; // if not used at the end of the project, we can remove the import
-
+import Instrument.*;
+import Person.*;
+// if not used at the end of the project, we can remove the import
+/*
+* The main class is used to verify that the whole project is working without errors
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
