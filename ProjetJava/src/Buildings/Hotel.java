8c793de28494e5010bb14ed20e70cf728d197311
AlexiFa <113013323+AlexiFa@users.noreply.github.com>
2022-12-11 16:04:42 +0100

    stars

--- ProjetJava/src/Buildings/Hotel.java
+++ ProjetJava/src/Buildings/Hotel.java
@@ -5,14 +5,18 @@ public class Hotel extends Building {
     int nbStars;
     int nbRoom;
     int nbSpa; // only for 4 stars hotel (else = 0)
-    int nbPool; // only for 5 stars hotel (else = 0)
+    int nbPool; // only for 5 or 4 stars hotel (else = 0)
     int nbSuite; // idem for 5 stars
     public Hotel(){
     }
     public Hotel(int stars){
         nbStars = stars;
         switch (nbStars){
-            case 1 : nb
+            case 1 : nbRoom = 10;
+            case 2 : nbRoom = 15;
+            case 3 : nbRoom = 20;
+            case 4 :
+
         }
     }
     public Hotel(String address, float livingSpace, int nbRoom, Person owner) {
